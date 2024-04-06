475d4cf60e9313a3006968ab34e54f8e79168afe
msaadsaeed <57453957+msaadsaeed@users.noreply.github.com>
2024-04-06 13:51:56 +0500

    Add files via upload

---
+++ vggFaceFeat.py
@@ -0,0 +1,67 @@
+
+import numpy as np
+from keras.preprocessing import image
+from tensorflow.keras.models import Sequential,Model
+from tensorflow.keras.layers import ZeroPadding2D,Convolution2D,MaxPooling2D
+from tensorflow.keras.layers import Dropout,Flatten
+from keras.applications.vgg16 import preprocess_input
+from glob import glob
+import progressbar
+from pickle import dump
+import os
+
+def create_model():
+    model = Sequential()
+    model.add(ZeroPadding2D((1,1),input_shape=(224,224, 3)))
+    model.add(Convolution2D(64, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(ZeroPadding2D((1,1)))	
+    model.add(Convolution2D(128, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(Convolution2D(4096, (7, 7), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(4096, (1, 1), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(2622, (1, 1)))
+    model.add(Flatten())
+    
+    model.load_weights('vgg_face_weights.h5')
+    vgg_face = Model(inputs=model.layers[0].input,outputs=model.layers[-3].output)
+    return vgg_face
+
+def extract_embed(img):
+    img = image.load_img(img, target_size=(224, 224))
+    img_data = image.img_to_array(img)
+    img_data = np.expand_dims(img_data, axis=0)
+    img_data = preprocess_input(img_data)
+    feat = vgg_face.predict(img_data)
+    return feat
+
+vgg_face = create_model()
+feat = extract_embed(path_to_img)
+
